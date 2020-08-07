4b37099ea0d7d4a651758518be37a56fc7404ce5
Shahid Fazaal <shahidfazaal@gmail.com>
2020-08-08 00:06:40 +0530

    code UserDAOImpl

--- src/dao/custom/impl/UserDAOImpl.java
+++ src/dao/custom/impl/UserDAOImpl.java
@@ -1,33 +1,45 @@
 package dao.custom.impl;
 
+import dao.CrudUtil;
 import dao.custom.UserDAO;
 import entity.User;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAOImpl implements UserDAO {
     @Override
     public List<User> findAll() throws Exception {
-        return null;
+        ResultSet rst = CrudUtil.execute("SELECT * FROM `User`");
+        ArrayList<User> users = new ArrayList<>();
+        while (rst.next()){
+            users.add(new User(rst.getString(1),rst.getString(2),rst.getString(3),rst.getDate(4),rst.getString(5),rst.getString(6),rst.getString(7),rst.getString(8)));
+        }
+        return users;
     }
 
     @Override
     public User find(String pk) throws Exception {
+        ResultSet rst = CrudUtil.execute("SELECT * FROM `User` WHERE userId=?",pk);
+        if (rst.next()){
+            return new User(rst.getString(1),rst.getString(2),rst.getString(3),rst.getDate(4),rst.getString(5),rst.getString(6),rst.getString(7),rst.getString(8));
+        }
         return null;
     }
 
     @Override
     public boolean save(User entity) throws Exception {
-        return false;
+        return CrudUtil.execute("INSERT INTO `User` VALUES (?,?,?,?,?,?,?,?)",entity.getUserId(),entity.getUserName(),entity.getNic(),entity.getDob(),entity.getPassword(),entity.getEmailAddress(),entity.getBranch(),entity.getUserRole());
     }
 
     @Override
     public boolean update(User entity) throws Exception {
-        return false;
+        return CrudUtil.execute("UPDATE `User` SET username=?,nic=?,dob=?,password=?,emailAddress=?,branch=?,userRole=? WHERE userId=?",entity.getUserName(),entity.getNic(),entity.getDob(),entity.getPassword(),entity.getEmailAddress(),entity.getBranch(),entity.getUserRole(),entity.getUserId());
     }
 
     @Override
     public boolean delete(String key) throws Exception {
-        return false;
+        return CrudUtil.execute("DELETE FROM `User` WHERE userId=?",key);
     }
 }
